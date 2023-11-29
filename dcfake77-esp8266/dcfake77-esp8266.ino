cfa3999cdaaad636d8cdbbd7cc93bc1bb492a4f8
Luigi Calligaris <calligar@ncc.unesp.br>
2023-11-29 01:50:43 -0300

    Add proper license header

--- dcfake77-esp8266/dcfake77-esp8266.ino
+++ dcfake77-esp8266/dcfake77-esp8266.ino
@@ -1,10 +1,26 @@
+// dcfake77-esp8266
+// Copyright (C) 2018-2023  Luigi Calligaris
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <ESP8266WiFi.h>
 #include "time.h"
 #include "dcf77protocol.h"
 // #include "dcf77protocol.c"
 
-// const char* wifi_ssid   = "SSID";
-// const char* wifi_pass   = "pass";
+// const char* wifi_ssid   = "SET_YOUR_SSID";
+// const char* wifi_pass   = "SET_YOUR_PASS";
 
 //const char* ntp_server          = "pool.ntp.org";
 const char* ntp_server          = "it.pool.ntp.org";
